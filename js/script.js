fd46bff612bfcbcabb4444d1fc9ddb74ce928fe8
FlatRiver <kpk0416@gmail.com>
2022-04-06 13:11:35 +0900

    2번째 페이지 작성중

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
 
+    // a 태그 클릭시 상단으로 이동하는 기능 블록
+    $("a[href='#']").click(function(e){
+        e.preventDefault();
+    });
+
     $(".fullpage").fullpage({
 
     })
@@ -14,7 +19,16 @@ $(document).ready(function(){
         $(".main-slider-text-box").slick("slickGoTo", nextSlide)
         $(".main-slider-text-box .main-text-box").find("p").removeClass("active")
         $(".main-slider-text-box .main-text-box").eq(nextSlide+1).find("p").addClass("active")
+        $(".main-box > .control-box > .page-box > .page-count").text("0"+(nextSlide+1))
     });
+
+    $(".main-box > .control-box > .left-btn").click(function(){
+        $(".main-slider").slick("slickPrev")
+    })
+
+    $(".main-box > .control-box > .right-btn").click(function(){
+        $(".main-slider").slick("slickNext")
+    })
 
     $(".main-slider-text-box").slick({
         arrows:false,
